9021b236983e9d1102c39f320dc414303f7d2293
TarunVanshkar <tarunben33@gmail.com>
2023-06-28 21:54:00 +0530

    Simple Greeting Page React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let currDate = new Date();
+// let currDate = new Date(2023, 6, 28, 5);  // To give custom time(year, month, date, hour)
+currDate = currDate.getHours();
+let greeting = "";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// To style greeting part only => we need to have inline style
+let cssStyle = {};
+if(currDate >= 1 && currDate<12){
+  greeting = "Good Morning";
+  cssStyle.color = "green";
+}
+else if(currDate >= 12 && currDate<19){
+  greeting = "Good Afternoon";
+  cssStyle.color = "orange";
+}
+else{
+  greeting = "Good Night";
+  cssStyle.color = "black";
+}
+
+
+ReactDOM.render(<>
+                  <div>
+                    <h1>Hello Sir, <span style={cssStyle}>{greeting}</span></h1>
+                  </div> 
+                </>, document.getElementById("root"));
